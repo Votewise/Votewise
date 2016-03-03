d455a5e4804ecca7662d0f4675b5f49df500ae24
james foley <kingdomcrawl@gmail.com>
2016-03-03 13:36:23 -0500

    Added process.env.PORT for node server

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.post('/politicianList', PoliticianList.postPoliticianList);
 app.put('/politicianList', PoliticianList.putPoliticianList);
 app.get('/politicianList/:userId', PoliticianList.getPoliticianList);
 
-var server = app.listen(config.port, function() {
+var server = app.listen(process.env.PORT || config.port, function() {
     var address = server.address();
 
     console.log('Example app listening at http://%s:%s',
